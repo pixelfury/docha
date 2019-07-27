38f3859d38bbc2046d22aee545ec90a73ee2dc7e
pixelfury <52672501+pixelfury@users.noreply.github.com>
2019-07-27 20:02:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -57,17 +57,6 @@ class DochaApp(BoxLayout):
 class DochkaApp(App):
     def build(self):
         return DochaApp()
-       
-def reset():
-    import kivy.core.window as window
-    from kivy.base import EventLoop
-    if not EventLoop.event_listeners:
-        from kivy.cache import Cache
-        window.Window = window.core_select_lib('window', window.window_impl, True)
-        Cache.print_usage()
-        for cat in Cache._categories:
-            Cache._objects[cat] = {}
 
 if __name__ == '__main__':
-   reset()
    DochkaApp().run()
